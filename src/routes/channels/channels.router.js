ecd8e58cb6404008c2abf2154ed42aa3f65fd16e
Florence Shelley <florence@serindlabs.com>
2019-09-06 19:26:06 -0400

    Set up channels router and controller

---
+++ src/routes/channels/channels.router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const { channelsController } = require('./channels.controller')
+
+const router = express.Router()
+
+router.get('', channelsController)
+
+module.exports = {
+  channelsRouter: router
+}

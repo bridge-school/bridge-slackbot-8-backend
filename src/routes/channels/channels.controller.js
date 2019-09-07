cfd16913f2d63b27b330ca723f9b7f9c7cce89dc
Florence Shelley <florence@serindlabs.com>
2019-09-06 22:11:49 -0400

    Remove redundant catch and simplify

--- src/routes/channels/channels.controller.js
+++ src/routes/channels/channels.controller.js
@@ -2,14 +2,11 @@ const axios = require('axios')
 
 const channelsController = async (req, res, next) => {
   const params = `exclude_archived=true&types=public_channel&token=${process.env.SLACK_AUTH_TOKEN}`
+  const url = `https://slack.com/api/conversations.list?${params}`
 
   try {
-    await axios(`https://slack.com/api/conversations.list?${params}`)
-      .then(response => {
-        const { channels } = response.data
-        res.send(channels)
-      })
-      .catch(err => next(err))
+    const channels = await axios(url).then(res => res.data.channels)
+    res.send(channels)
   } catch (err) {
     next(err)
   }

68deb8e8efaa56f7d802e3a36c5b4fb939380be8
Florence Shelley <23623312+florenceshelley@users.noreply.github.com>
2019-09-07 16:18:34 -0400

    [Feature][201] Fetch channels (#6)
    * [Chore][001] Add shared configs (#2)
    
    * Install eslint, eslint-config-airbnb, prettier, and create shared config files
    
    * Reformat files using shared configs for consistency
    
    * Install dotenv and request libraries
    
    * Set up channels router and controller
    
    * Ignore .env.dev
    
    * Remove request library and add axios
    
    * Refactor controller to use axios, and handle error
    
    * Remove redundant catch and simplify
    
    * Add missing step of only returning id and name
    
    * Set cors origin based on app environment
    
    * Create utility to convert params object to a string for better readability
    
    * Refactor parametize utility

--- src/routes/channels/channels.controller.js
+++ src/routes/channels/channels.controller.js
@@ -1,24 +1,21 @@
-const request = require('request')
+const axios = require('axios')
+const { parametize } = require('../../utils/parametize')
 
-const channelsController = (req, res, next) => {
-  new Promise((resolve, reject) => {
-    request(
-      `https://slack.com/api/conversations.list?exclude_archived=true&types=public_channel&token=${process.env.SLACK_AUTH_TOKEN}`,
-      (req, res) => {
-        const { body } = res
-        resolve(body)
-      }
-    )
-  }).then(response => {
-    const list = JSON.parse(response)
-
-    const channels = list.channels.map(channel => ({
-      id: channel.id,
-      name: channel.name
-    }))
-
-    res.send(channels)
+const channelsController = async (req, res, next) => {
+  const params = parametize({
+    exclude_archived: true,
+    types: 'public_channel',
+    token: process.env.SLACK_AUTH_TOKEN
   })
+  const url = `https://slack.com/api/conversations.list?${params}`
+
+  try {
+    const response = await axios(url).then(res => res.data.channels)
+    const channels = response.map(({ id, name }) => ({ id, name }))
+    res.send(channels)
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {

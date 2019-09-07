5bafde6b56a7b875f6b712920bd9c74e7a4cde5d
Florence Shelley <florence@serindlabs.com>
2019-09-06 21:55:58 -0400

    Refactor controller to use axios, and handle error

--- src/routes/channels/channels.controller.js
+++ src/routes/channels/channels.controller.js
@@ -1,24 +1,18 @@
-const request = require('request')
+const axios = require('axios')
 
-const channelsController = (req, res, next) => {
-  new Promise((resolve, reject) => {
-    request(
-      `https://slack.com/api/conversations.list?exclude_archived=true&types=public_channel&token=${process.env.SLACK_AUTH_TOKEN}`,
-      (req, res) => {
-        const { body } = res
-        resolve(body)
-      }
-    )
-  }).then(response => {
-    const list = JSON.parse(response)
+const channelsController = async (req, res, next) => {
+  const params = `exclude_archived=true&types=public_channel&token=${process.env.SLACK_AUTH_TOKEN}`
 
-    const channels = list.channels.map(channel => ({
-      id: channel.id,
-      name: channel.name
-    }))
-
-    res.send(channels)
-  })
+  try {
+    await axios(`https://slack.com/api/conversations.list?${params}`)
+      .then(response => {
+        const { channels } = response.data
+        res.send(channels)
+      })
+      .catch(err => next(err))
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {

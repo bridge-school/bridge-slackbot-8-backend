2eb52ba843dd7afafb0438869f97fb4dae80c666
tunimosmann <rossimosmann@gmail.com>
2019-09-14 15:33:03 -0400

    reverted poll to the normal path, added comment about changes for ngrok to work with localhost

--- src/api/index.js
+++ src/api/index.js
@@ -7,12 +7,9 @@ const { pollRouter } = require('../routes/poll/poll.router')
 const router = express.Router()
 router.use('/health', healthRouter)
 router.use('/channels', channelsRouter)
+router.use('/poll', pollRouter)
 
-// !!! change to /poll after we have the real URL !!!
-// router.use('/poll', pollRouter)
-
-// This is just for ngrok to work
-router.use('/', pollRouter)
-
+// This is just for ngrok to work -- comment out the normal path for poll -- router.use('/poll', pollRouter) and use the following path:
+// router.use('/', pollRouter)
 
 module.exports = router

ad087f78d3c736ad907431217ac7417c86b90b5e
gerrywu1990 <gerrywu1990@gmail.com>
2019-09-14 22:28:28 -0400

    Integrated with slack api in addPollsController to save new poll to db and send poll to slack

--- src/routes/polls/polls.controller.js
+++ src/routes/polls/polls.controller.js
@@ -1,12 +1,36 @@
+const axios = require('axios')
+const { parametize } = require('../../utils/parametize')
 const db = require('../../db')
+
+const createSlackBlock = require('../../utils/createSlackBlock')
 const mockResponse = require('./mockResponse')
 
+const sendPolltoSlack = async (interactionBlock, channel) => {
+  const params = parametize({
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: channel,
+    text: 'Poll created',
+    blocks: JSON.stringify(interactionBlock)
+  })
+
+  const url = `https://slack.com/api/chat.postMessage?${params}`
+
+  try {
+    axios.post(url).then(res => {
+      console.log('send poll to slack successfully')
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
 const addPollsController = async (req, res) => {
-  let testData = {
-    question: 'TEST question',
+  const { question, channel_name, channel_id } = req.body
+  const newPoll = {
+    question: question,
     channel: {
-      name: 'testChannel',
-      id: 001
+      name: channel_name,
+      id: channel_id
     },
     response: {
       yes: 0,
@@ -16,11 +40,13 @@ const addPollsController = async (req, res) => {
   }
 
   db.collection('polls')
-    .doc(req.params.id)
-    .set(testData)
+    .add(newPoll)
     .then(docRef => {
-      console.log('docRef', docRef)
-      res.status(200).send('Poll successfully created')
+      const block = createSlackBlock(docRef.id, question)
+      sendPolltoSlack(block, channel_id) &&
+        res
+          .status(200)
+          .json({ id: docRef.id, message: 'Poll successfully created' })
     })
     .catch(error => {
       res.json({ error })
--- src/routes/polls/polls.router.js
+++ src/routes/polls/polls.router.js
@@ -10,7 +10,7 @@ const {
 const router = express.Router()
 
 router.get('/', getPollsController)
-router.post('/:id', addPollsController)
+router.post('/', addPollsController)
 router.put('/:id', updatePollsController)
 router.delete('/:id', deletePollsController)
 

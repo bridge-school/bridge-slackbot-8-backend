2af77e6ea8963bf51f428c5fa487e749f900be7c
gerrywu1990 <gerrywu1990@gmail.com>
2019-09-15 20:42:56 -0400

    remove poll route from api index file

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,10 @@ const express = require('express')
 const { healthRouter } = require('../routes/health/health.router')
 const { channelsRouter } = require('../routes/channels/channels.router')
 const { pollsRouter } = require('../routes/polls/polls.router')
-const { pollRouter } = require('../routes/poll/poll.router')
 
 const router = express.Router()
 router.use('/health', healthRouter)
 router.use('/channels', channelsRouter)
-router.use('/poll', pollRouter)
 router.use('/polls', pollsRouter)
 
 // This is just for ngrok to work -- comment out the normal path for poll -- router.use('/poll', pollRouter) and use the following path:
